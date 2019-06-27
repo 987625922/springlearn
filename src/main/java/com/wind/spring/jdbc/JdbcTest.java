b2cd84d84cf803cdf52960fb3d6ccce69640095c
wind <987625922@qq.com>
2019-06-27 18:24:58 +0800

    学习AOP

--- src/main/java/com/wind/spring/jdbc/JdbcTest.java
+++ src/main/java/com/wind/spring/jdbc/JdbcTest.java
@@ -1,25 +1,57 @@
 package com.wind.spring.jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class JdbcTest {
 
     public static void main(String args[]) throws ClassNotFoundException, SQLException {
-        String sql = "select * from book";
         Connection con = null;
         Statement stmt = null;
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        //连接对象
-        con = DriverManager.getConnection("jdbc:mysql:///ssm?useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT%2B8", "root", "root");
-        //执行命令对象
-        stmt = con.createStatement();
-        stmt.execute(sql);
+        ResultSet resultSet = null;
+        try {
+            String sql = "select * from book";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //连接对象
+            con = DriverManager.getConnection("jdbc:mysql:///ssm?useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT%2B8", "root", "root");
+            //执行命令对象
+            stmt = con.createStatement();
+            //执行sql语句,拿到结果集
+            resultSet = stmt.executeQuery("SELECT * FROM book");
 
-        stmt.close();
-        con.close();
+            //遍历结果集，得到数据
+            while (resultSet.next()) {
+
+                System.out.println(resultSet.getString(1));
+
+                System.out.println(resultSet.getString(2));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }finally {
+            if (resultSet != null){
+                try {
+                    resultSet.close();
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+            if (stmt != null){
+                try {
+                    stmt.close();
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+            if (con != null){
+                try {
+                    con.close();
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 }
