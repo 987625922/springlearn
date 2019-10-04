92ca417692bbd3bcb7bc578de76d4c541bb78c90
98762 <987625922@qq.com>
2019-10-04 16:43:54 +0800

    学习Servlet

--- src/main/java/com/wind/java/servlet/HttpServlet.java
+++ src/main/java/com/wind/java/servlet/HttpServlet.java
@@ -4,15 +4,28 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 public class HttpServlet extends javax.servlet.http.HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+//        super.doGet(req, resp);
+//        System.out.println(req.getParameterNames());
+        Enumeration pNames=req.getParameterNames();
+        while(pNames.hasMoreElements()){
+            String name=(String)pNames.nextElement();
+            String value=req.getParameter(name);
+            System.out.println(name + "=" + value);
+        }
+
+        System.out.println(req.getParameter("username"));
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+//        super.doPost(req, resp);
+        System.out.println(req.getParameterNames());
+        System.out.println(req.getParameter("username"));
     }
 }
