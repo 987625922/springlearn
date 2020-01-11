d14d1cdbc0b9e63aed588d48f993fe6a829fdffa
wind <987625922@qq.com>
2020-01-11 17:55:48 +0800

    SpringMVC之请求参数的获取方式-POST请求获取

--- src/main/java/com/wind/spring/web/controller/ParamController.java
+++ src/main/java/com/wind/spring/web/controller/ParamController.java
@@ -21,7 +21,7 @@ public class ParamController {
      * @param request
      * @return
      */
-    @RequestMapping("/get1")
+    @RequestMapping(path = "/get1", method = RequestMethod.GET)
     public String getParam1(HttpServletRequest request) {
         String user = request.getParameter("user");
         String password = request.getParameter("password");
@@ -37,7 +37,7 @@ public class ParamController {
      * @param password
      * @return
      */
-    @RequestMapping(path = "/get2")
+    @RequestMapping(path = "/get2", method = RequestMethod.GET)
     public String getParam2(String user, String password) {
         return "getParam2 user: " + user + " pwd: " + password;
     }
@@ -77,13 +77,86 @@ public class ParamController {
      * @param userName
      * @return
      */
-    @RequestMapping(path = "get5/{userName}/info")
+    @RequestMapping(path = "get5/{userName}/info", method = RequestMethod.GET)
     public String req6(@PathVariable(name = "userName") String userName) {
         return "get5 user: " + userName;
     }
 
     /** =================================== get ============= */
 
+    /** =========================== POST请求参数获取 ========================= */
 
+    /**
+     * 对于HttpServletReuqest方式获取参数时，get和post没什么区别
+     * 1.http://localhost:8080/params/post1?user=coco&password=123
+     * <p>
+     * 2.http://localhost:8080/params/post1
+     * Content-Type: application/x-www-form-urlencoded
+     * 在x-www-form-urlencoded
+     * user=12
+     * password=123
+     */
+    @PostMapping("/post1")
+    public String post1(HttpServletRequest request) {
+        String user = request.getParameter("user");
+        String password = request.getParameter("password");
+        return "post1 user: " + user + " pwd: " + password;
+    }
+
+    /***
+     * 1.http://localhost:8080/params/post1?user=coco&password=123
+     *
+     * 2.http://localhost:8080/params/post1
+     * Content-Type: application/x-www-form-urlencoded
+     * 在x-www-form-urlencoded
+     *  user=12
+     *  password=123
+     *
+     * @param user
+     * @param password
+     * @return
+     */
+    @PostMapping("/post2")
+    public String post2(String user, String password) {
+        return "post2 user: " + user + " pwd: " + password;
+    }
+
+    /**
+     * 1.http://localhost:8080/params/post1?user=coco&password=123
+     * <p>
+     * 2.http://localhost:8080/params/post1
+     * Content-Type: application/x-www-form-urlencoded
+     * 在x-www-form-urlencoded
+     * user=12
+     * password=123
+     *
+     * @param username
+     * @param pwd
+     * @return
+     */
+    @PostMapping("/post3")
+    public String post3(@RequestParam("user") String username,
+                        @RequestParam("password") String pwd) {
+        return "post3 user: " + username + " pwd: " + pwd;
+    }
+
+    /**
+     * 1.http://localhost:8080/params/post4?name=coco&id=123
+     * <p>
+     * 2.http://localhost:8080/params/post4
+     * Content-Type: application/x-www-form-urlencoded
+     * 在x-www-form-urlencoded
+     * id=12
+     * name=123
+     *
+     * @param book
+     * @return
+     */
+    @PostMapping(path = "post4")
+    public String post4(Book book) {
+        return "post4 book: " + book;
+    }
+
+    /** =========================== post ========================= */
 
 }
