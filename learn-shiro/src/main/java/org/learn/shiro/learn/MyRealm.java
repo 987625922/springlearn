f08fd41bda2dc2542da640012ca13c0dbb9fbf65
98762 <987625922@qq.com>
2020-06-10 22:32:22 +0800

    更新笔记

---
+++ learn-shiro/src/main/java/org/learn/shiro/learn/MyRealm.java
@@ -0,0 +1,42 @@
+package org.learn.shiro.learn;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * 从Realm获取安全数据（如用户、角色、权限），
+ * 就是说SecurityManager要验证用户身份，
+ * 那么它需要从Realm获取相应的用户进行比较以确定用户身份是否合法；
+ * 也需要从Realm得到用户相应的角色/权限进行验证用户是否能进行操作
+ */
+public class MyRealm implements Realm {
+
+    /**
+     * 返回一个唯一的Realm名字
+     */
+    @Override
+    public String getName() {
+        return "myrealm";
+    }
+    //判断此Realm是否支持此Token
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        //仅支持UsernamePasswordToken类型的Token
+        return token instanceof UsernamePasswordToken;
+    }
+
+    //根据Token获取认证信息
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = (String)token.getPrincipal();  //得到用户名
+        String password = new String((char[])token.getCredentials()); //得到密码
+        if(!"123".equals(username)) {
+            throw new UnknownAccountException(); //如果用户名错误
+        }
+        if(!"123".equals(password)) {
+            throw new IncorrectCredentialsException(); //如果密码错误
+        }
+        //如果身份认证验证成功，返回一个AuthenticationInfo实现；
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+}
