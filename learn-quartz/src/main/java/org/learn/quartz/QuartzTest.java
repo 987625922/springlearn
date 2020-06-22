cb60e5d919d7beb20771b58dbc37b2f44d3441f4
wind <987625922@qq.com>
2020-06-22 17:49:12 +0800

    quartz学习

--- learn-quartz/src/main/java/org/learn/quartz/QuartzTest.java
+++ learn-quartz/src/main/java/org/learn/quartz/QuartzTest.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Component;
 public class QuartzTest {
 
     //定时任务每天0点触发
-    @Scheduled(cron = "59 59 23 * * ?")
+//    @Scheduled(cron = "59 59 23 * * ?")
+    //每隔5秒触发
+    @Scheduled(cron = "*/5 * * * * ?")
     public void show() {
         System.out.println(System.currentTimeMillis() + "");
     }
