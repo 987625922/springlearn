6d1343d8c1e43a33bb80305f14206654d151f25a
longjingfei <longjingfei>
2021-10-14 17:56:14 +0800

    提交spring-clound

---
+++ spring-boot-learn/springboot-learn-activemq/src/main/java/com/learn/config/ProducerController.java
@@ -0,0 +1,44 @@
+package com.learn.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.jms.Destination;
+import javax.jms.Queue;
+import javax.jms.Topic;
+
+/**
+ * Dubbo 使用的controller
+ */
+@RestController
+public class ProducerController {
+
+    @Autowired
+    private JmsMessagingTemplate jmsMessagingTemplate;
+
+    @Autowired
+    private Queue queue;
+
+    @Autowired
+    private Topic topic;
+
+    @PostMapping("/queue/test")
+    public String sendQueue(@RequestBody String str) {
+        this.sendMessage(this.queue, str);
+        return "success";
+    }
+
+    @PostMapping("/topic/test")
+    public String sendTopic(@RequestBody String str) {
+        this.sendMessage(this.topic, str);
+        return "success";
+    }
+
+    // 发送消息，destination是发送到的队列，message是待发送的消息
+    private void sendMessage(Destination destination, final String message) {
+        jmsMessagingTemplate.convertAndSend(destination, message);
+    }
+}
