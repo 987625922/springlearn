1b8d4e0650915382e44b92f720a0c9975d7969e8
longjingfei <longjingfei>
2022-02-14 14:28:50 +0800

    添加future笔记

--- javalearn/src/main/src/main/java/basis/thread/future/CompletableFutureLearn.java
+++ javalearn/src/main/src/main/java/basis/thread/future/CompletableFutureLearn.java
@@ -8,7 +8,7 @@ import java.util.function.Supplier;
  * <p>
  * CompletableFuture提供了几十种方法，辅助我们的异步任务场景。
  * 这些方法包括创建异步任务、任务异步回调、多个任务组合处理 等方面。
- *
+ * <p>
  * 方法：
  * supplyAsync执行CompletableFuture任务，支持返回值
  * runAsync执行CompletableFuture任务，没有返回值。
@@ -51,12 +51,73 @@ public class CompletableFutureLearn {
                 }
                 return "2222";
             }
-        },executor);
+        }, executor);
 
         //  阻塞线程
         String userInfo = completableUserInfoFuture.get(2, TimeUnit.SECONDS);
         String medalInfo = completableMedalInfoFuture.get();
 
         System.out.println("总共用时" + (System.currentTimeMillis() - startTime) + "ms");
+
+        //  ========================= 执行完第一个任务，继续执行第二个任务
+        //  1.  最后的结果没有返回值
+        CompletableFuture<String> orgFuture = CompletableFuture.supplyAsync(
+                () -> {
+                    System.out.println("原始CompletableFuture方法任务");
+                    return "芋道源码";
+                }
+        );
+
+        CompletableFuture thenAcceptFuture = orgFuture.thenAccept((a) -> {
+            if ("芋道源码".equals(a)) {
+                System.out.println("关注了");
+            }
+
+            System.out.println("先考虑考虑");
+        });
+
+        System.out.println(thenAcceptFuture.get());
+
+        //  2.  最后的结果有返回值
+        CompletableFuture<String> orgFuture2 = CompletableFuture.supplyAsync(
+                ()->{
+                    System.out.println("原始CompletableFuture方法任务");
+                    return "芋道源码";
+                }
+        );
+
+        CompletableFuture<String> thenApplyFuture = orgFuture2.thenApply((a) -> {
+            if ("芋道源码".equals(a)) {
+                return "关注了";
+            }
+
+            return "先考虑考虑";
+        });
+
+        System.out.println(thenApplyFuture.get());
+
+        //  ======================  异常处理
+        //  CompletableFuture的exceptionally方法表示，某个任务执行异常时，执行的回调方法;
+        //  并且有抛出异常作为参数 ，传递到回调方法。
+        //  方法处理中，出现了异常会回调到这个方法
+        CompletableFuture<String> exceptionFuture = orgFuture.exceptionally((e) -> {
+            //  抛出异常信息
+            e.printStackTrace();
+            return "你的程序异常啦";
+        });
+        //  输出你的程序异常了
+        System.out.println(exceptionFuture.get());
+
+        //  =====================   监听任务完成
+        //  某个任务执行完成后，执行的回调方法，无返回值 ；
+        CompletableFuture<String> rstFuture = orgFuture.whenComplete((a, throwable) -> {
+            System.out.println("当前线程名称：" + Thread.currentThread().getName());
+            System.out.println("上个任务执行完啦，还把" + a + "传过来");
+            if ("芋道源码".equals(a)) {
+                System.out.println("666");
+            }
+            System.out.println("233333");
+        });
+
     }
 }
